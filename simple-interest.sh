baac1db1a9933615851f793e60d912b7e96ce09f
72venkatesh <119421776+72venkatesh@users.noreply.github.com>
2025-09-17 21:24:16 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,18 @@
+
+---
+
+### 5️⃣ simple-interest.sh
+```bash
+#!/bin/bash
+# Simple Interest Calculator
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate of Interest:"
+read r
+echo "Enter Time:"
+read t
+
+si=$(( (p * r * t) / 100 ))
+
+echo "Simple Interest = $si"
